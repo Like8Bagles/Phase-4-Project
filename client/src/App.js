fd5523bfdc0e1013dbe501944c78a42fae20865c
Anthony <anthonykmbanks@gmail.com>
2021-07-09 18:29:07 -0400

    add signup route

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,20 @@
 import './App.css';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch, useHistory } from 'react-router-dom'
+import React, { useState, useEffect } from 'react';
 import Home from './containers/Home.js'
 import Navigation from './components/Navigation'
 
 function App() {
+  const [loggedIn, setLoggedIn] = useState(false)
+  const [user, setUser] = useState({})
+  // const history = useHistory()
+
   return (
     <div className="App">
       <Navigation />
       <Switch>
         <Route exact path="/" component={Home}/>
+        <Route exact path="/signup" render={routerProps => <Signup {...routerProps} onSignup={onSignup} />} />
       </Switch>
     </div>
   );
